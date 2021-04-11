ccb640a48754aef12e3b27a41a5cf07c08c8b80f
yulduz <yulduz83@gmail.com>
2021-04-10 23:00:22 -0500

    friend and unfriend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const ERR_MSG_NO_USR_FOUND = "No user with this id was found";
+const ERR_MSG_NO_CONNECTION_FOUND = "No friend (user) with this id was found";
 
 const userController = {
   getAllUsers(req, res) {
@@ -64,6 +65,58 @@ const userController = {
           return;
         }
         res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  connectWithUser({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.friendId },
+      { $addToSet: { friends: params.userId } },
+      { runValidators: true, new: true }
+    )
+      .then((dbFriendUserData) => {
+        if (!dbFriendUserData) {
+          res.status(404).json({ message: ERR_MSG_NO_CONNECTION_FOUND });
+          return;
+        } else {
+          User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { runValidators: true, new: true }
+          ).then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: ERR_MSG_NO_USR_FOUND });
+              return;
+            }
+            res.json(dbUserData);
+          });
+        }
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  disconnectWithUser({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.friendId },
+      { $pull: { friends: params.userId } },
+      { runValidators: true, new: true }
+    )
+      .then((dbFriendUserData) => {
+        if (!dbFriendUserData) {
+          res.status(404).json({ message: ERR_MSG_NO_CONNECTION_FOUND });
+          return;
+        } else {
+          User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { runValidators: true, new: true }
+          ).then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: ERR_MSG_NO_USR_FOUND });
+              return;
+            }
+            res.json(dbUserData);
+          });
+        }
       })
       .catch((err) => res.status(400).json(err));
   },
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,11 +6,15 @@ const {
   getUserById,
   updateUser,
   deleteUser,
+  connectWithUser,
+  disconnectWithUser
 } = require("../../controllers/user-controller");
 
 // setup REST
 router.route("/").get(getAllUsers).post(createUser);
 
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:id").get(getUserById).post(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(connectWithUser).delete(disconnectWithUser)
 
 module.exports = router;
