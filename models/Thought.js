b3bc95255b9c5db43116ecfcaaeb50ceb40d938d
yulduz <yulduz83@gmail.com>
2021-04-11 20:48:13 -0500

    adds date formatter util

--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,35 @@
 const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const ReactSchema = new Schema({
-  reactionId: {
-    // set custom id to avoid confusion with parent thought_id
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
+const ReactSchema = new Schema(
+  {
+    reactionId: {
+      // set custom id to avoid confusion with parent thought_id
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: "reactionBody is a required field",
+      //TODO: 280 character maximum
+    },
+    username: {
+      type: String,
+      required: "username is a required field",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  reactionBody: {
-    type: String,
-    required: "reactionBody is a required field",
-    //TODO: 280 character maximum
-  },
-  username: {
-    type: String,
-    required: "username is a required field",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    //TODO: Use a getter method to format the timestamp on query
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 const ThoughtSchema = new Schema(
   {
@@ -32,7 +41,7 @@ const ThoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      //TODO: Use a getter method to format the timestamp on query
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     username: {
       type: String,
